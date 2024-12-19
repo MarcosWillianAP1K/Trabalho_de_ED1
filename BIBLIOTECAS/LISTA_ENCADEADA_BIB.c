f1306fbf446661b437473335db695150088248ae
MarcosWillianAP1K <mwss@outlook.com>
2024-12-19 15:38:30 -0300

    Biblioteca lista_encadeada

--- BIBLIOTECAS/LISTA_ENCADEADA_BIB.c
+++ BIBLIOTECAS/LISTA_ENCADEADA_BIB.c
@@ -2,17 +2,17 @@
 #include <time.h>
 #include <string.h>
 
-void *iniciar()
+// void *iniciar()
+// {
+//     return NULL;
+// }
+
+
+
+void adicionar_elemento(Lista_encadeada **lista, int valor)
 {
-    return NULL;
-}
-
-
-
-void adicionar_elemento(Lista **lista, int valor)
-{
-    Lista *novo_no = (Lista *)malloc(sizeof(Lista));
-    novo_no->numero = valor;
+    Lista_encadeada *novo_no = (Lista_encadeada *)malloc(sizeof(Lista_encadeada));
+    // novo_no->numero = valor;
     novo_no->proximo = NULL;
 
     if (*lista == NULL)
@@ -21,55 +21,7 @@ void adicionar_elemento(Lista **lista, int valor)
     }
     else
     {
-        Lista *atual = *lista;
-
-        while (atual->proximo != NULL)
-        {
-            atual = atual->proximo;
-        }
-
-        atual->proximo = novo_no;
-    }
-}
-
-void adicionar_nomes(Lista_de_strings **lista, char *string)
-{
-    Lista_de_strings *novo_no = (Lista_de_strings *)malloc(sizeof(Lista_de_strings));
-    strcpy(novo_no->string, string);
-    novo_no->proximo = NULL;
-
-    if (*lista == NULL)
-    {
-        *lista = novo_no;
-    }
-    else
-    {
-        Lista_de_strings *atual = *lista;
-
-        while (atual->proximo != NULL)
-        {
-            atual = atual->proximo;
-        }
-
-        atual->proximo = novo_no;
-    }
-
-}
-
-void adicionar_vetor(Lista_vetores **lista, int *vetor)
-{
-    Lista_vetores *novo_no = (Lista_vetores *)malloc(sizeof(Lista_vetores));
-
-    novo_no->vetor = vetor;
-    novo_no->proximo = NULL;
-
-    if (*lista == NULL)
-    {
-        *lista = novo_no;
-    }
-    else
-    {
-        Lista_vetores *atual = *lista;
+        Lista_encadeada *atual = *lista;
 
         while (atual->proximo != NULL)
         {
@@ -82,9 +34,10 @@ void adicionar_vetor(Lista_vetores **lista, int *vetor)
 
 
 
-void criar_lista(Lista **lista, int tam)
+
+void criar_lista(Lista_encadeada **lista, int tam)
 {
-    Lista **atual = lista;
+   Lista_encadeada **atual = lista;
 
     for (int i = 0; i < tam; i++)
     {
@@ -93,9 +46,9 @@ void criar_lista(Lista **lista, int tam)
     }
 }
 
-void criar_lista_aleatoria(Lista **lista, int tam)
+void criar_lista_aleatoria(Lista_encadeada **lista, int tam)
 {
-    Lista **atual = lista;
+    Lista_encadeada **atual = lista;
 
     srand(time(NULL));
 
@@ -108,37 +61,9 @@ void criar_lista_aleatoria(Lista **lista, int tam)
 
 
 
-void liberar_memoria(Lista *lista)
+void liberar_memoria(Lista_encadeada *lista)
 {
-    Lista *anterior = lista;
-
-    while (lista != NULL)
-    {
-        lista = lista->proximo;
-        free(anterior);
-        anterior = lista;
-    }
-
-    free(anterior);
-}
-
-void liberar_memoria_nomes(Lista_de_strings *lista)
-{
-    Lista_de_strings *anterior = lista;
-
-    while (lista != NULL)
-    {
-        lista = lista->proximo;
-        free(anterior);
-        anterior = lista;
-    }
-
-    free(anterior);
-}
-
-void liberar_memoria_vetor(Lista_vetores *lista)
-{
-    Lista_vetores *anterior = lista;
+    Lista_encadeada *anterior = lista;
 
     while (lista != NULL)
     {
@@ -152,53 +77,24 @@ void liberar_memoria_vetor(Lista_vetores *lista)
 
 
 
-void printar_lista(Lista *list)
+
+void printar_lista(Lista_encadeada *list)
 {
     while (list != NULL)
     {
-        printf("%d ", list->numero);
+        
         list = list->proximo;
     }
 }
 
-void printar_lista_nomes(Lista_de_strings *list)
-{
-    while (list != NULL)
-    {
-        printf("%s  ", list->string);
-        list = list->proximo;
-    }
-}
 
-void printar_lista_modificado(Lista *list)
+void printar_lista_modificado(Lista_encadeada *list)
 {
     while (list != NULL)
     {
-        printf("\nvalor: %d\n", list->numero);
         printf("Endereco atual: %p\n", list);
         printf("Aponta: %p\n", list->proximo);
         list = list->proximo;
     }
 }
 
-void printar_lista_vetor(Lista_vetores *list)
-{
-    while (list != NULL)
-    {
-        int tam = sizeof(list->vetor) - 1;
-        printf("[");
-     
-        for (int i = 0; i < tam; i++)
-        {
-            printf("%d", list->vetor[i]);
-            
-            if (i != tam -1)
-            {
-                printf(", ");
-            }
-        }
-        printf("]  ");
-        
-        list = list->proximo;
-    }
-}
--- BIBLIOTECAS/LISTA_ENCADEADA_BIB.h
+++ BIBLIOTECAS/LISTA_ENCADEADA_BIB.h
@@ -1,57 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Lista_vetores
+
+
+typedef struct Lista_encadeada
 {
-    int *vetor;
-    struct Lista_vetores *proximo;
-} Lista_vetores;
+    signed short int ID;
+    char *nome;
 
+    signed short int nivel_prioridade;
 
-typedef struct Lista
-{
-    int numero;
-    struct Lista *proximo;
-} Lista;
+    signed short int minuto;
+    signed short int hora;
+    signed short int dia;
+    signed short int mes;
+    signed short int ano;
 
-typedef struct Lista_de_strings
-{
-    char string[20];
-    struct Lista_de_strings *proximo;
-} Lista_de_strings;
+    struct Lista_encadeada *proximo;
+} Lista_encadeada;
 
 
 
-void *iniciar();
+
+// void *iniciar();
 
 
 
-void adicionar_elemento(Lista **lista, int valor);
-
-void adicionar_nomes(Lista_de_strings **lista, char *string);
-
-void adicionar_vetor(Lista_vetores **lista, int *vetor);
+void adicionar_elemento(Lista_encadeada **lista, int valor);
 
 
 
-void criar_lista(Lista **lista, int tam);
 
-void criar_lista_aleatoria(Lista **lista, int tam);
+void criar_lista(Lista_encadeada **lista, int tam);
 
-
-
-void liberar_memoria(Lista **lista);
-
-void liberar_memoria_nomes(Lista_de_strings **lista);
-
-void liberar_memoria_vetor(Lista_vetores **lista);
+void criar_lista_aleatoria(Lista_encadeada **lista, int tam);
 
 
 
-void printar_lista(Lista *list);
+void liberar_memoria(Lista_encadeada **lista);
 
-void printar_lista_nomes(Lista_de_strings *list);
 
-void printar_lista_modificado(Lista *list);
 
-void printar_lista_vetor(Lista_vetores *list);
+void printar_lista(Lista_encadeada *list);
+
+void printar_lista_modificado(Lista_encadeada *list);
+
