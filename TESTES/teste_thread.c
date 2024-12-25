c44873569140d5cef9a3a73bb694d9071167e057
MarcosWillianAP1K <mwss@outlook.com>
2024-12-25 16:31:55 -0300

    Testes threads

--- TESTES/teste_thread.c
+++ TESTES/teste_thread.c
@@ -1,44 +1,35 @@
 #include <pthread.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#define NUM_THREADS 5
-
-pthread_mutex_t mutex; // Mutex para sincronização
-int counter = 0;       // Variável compartilhada
-
-void* thread_function(void* arg) {
-    int id = *(int*)arg;
-
-    pthread_mutex_lock(&mutex);
-    counter++; // Incrementa a variável compartilhada
-    printf("Thread %d: Counter = %d\n", id, counter);
-    pthread_mutex_unlock(&mutex);
-
-    return NULL;
+void* calculate_sum(void* arg) {
+    int n = *(int*)arg;
+    int* result = malloc(sizeof(int)); // Alocando memória para o resultado
+    *result = (n * (n + 1)) / 2; // Soma dos primeiros n números
+    return (void*)result;
 }
 
 int main() {
-    pthread_t threads[NUM_THREADS];
-    int thread_ids[NUM_THREADS];
-    int i;
+    pthread_t thread;
+    int n = 10;
+    int *sum;
 
-    pthread_mutex_init(&mutex, NULL); // Inicializa o mutex
-
-    for (i = 0; i < NUM_THREADS; i++) {
-        thread_ids[i] = i + 1;
-        if (pthread_create(&threads[i], NULL, thread_function, &thread_ids[i]) != 0) {
-            printf("Erro ao criar a thread %d\n", i + 1);
-            exit(1);
-        }
+    // Criando a thread
+    if (pthread_create(&thread, NULL, calculate_sum, (void*)&n) != 0) {
+        printf("Erro ao criar a thread\n");
+        return 1;
     }
 
-    for (i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
+    // Esperando a thread terminar e capturando o resultado
+    if (pthread_join(thread, (void**)&sum) != 0) {
+        printf("Erro ao esperar pela thread\n");
+        return 1;
     }
 
-    pthread_mutex_destroy(&mutex); // Destroi o mutex
-    printf("Todas as threads terminaram.\n");
+    printf("Soma dos primeiros %d numeros: %d\n", n, *sum);
+
+    // Liberando a memória alocada pela thread
+    free(sum);
 
     return 0;
 }
